f0233b44bb44c6677fb0084898ea51da341c041d
Mikhail <160492912+Goes2244@users.noreply.github.com>
2025-11-26 12:23:53 +0500

    Update Time.cs

--- Tasks_6-7/Time.cs
+++ Tasks_6-7/Time.cs
@@ -1,166 +1,160 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
-namespace lab4_2
+namespace lab4_1
 {
-    public class Time
+    public static class StaticTasks
     {
-        private byte _hours;
-        private byte _minutes;
-        
-        public byte Hours 
-        { 
-            get => _hours;
-            private set
+        // ЗАДАНИЕ 1.7 - List
+        public static void MoveFirstToEnd<T>(List<T> list)
+        {
+            if (list == null || list.Count == 0)
+                return;
+                
+            T firstElement = list[0];
+            list.RemoveAt(0);
+            list.Add(firstElement);
+        }
+
+        // ЗАДАНИЕ 2.7 - LinkedList
+        public static void RemoveElementsWithEqualNeighbors<T>(LinkedList<T> list)
+        {
+            if (list == null || list.Count < 3)
+                return;
+
+            var nodesToRemove = new List<LinkedListNode<T>>();
+            var current = list.First;
+
+            while (current != null)
             {
-                if (value >= 24)
-                    throw new ArgumentException("Часы должны быть в диапазоне 0-23");
-                _hours = value;
+                var previous = current.Previous ?? list.Last;
+                var next = current.Next ?? list.First;
+
+                if (previous.Value.Equals(next.Value))
+                {
+                    nodesToRemove.Add(current);
+                }
+
+                current = current.Next;
+            }
+
+            foreach (var node in nodesToRemove)
+            {
+                list.Remove(node);
             }
         }
 
-        public byte Minutes 
-        { 
-            get => _minutes;
-            private set
+        // ЗАДАНИЕ 3.7 - HashSet (фабрики)
+        public static (IEnumerable<string> purchasedByAll, IEnumerable<string> purchasedBySome, IEnumerable<string> purchasedByNone) 
+            AnalyzeFurniturePurchases(HashSet<string> allFactories, Dictionary<string, HashSet<string>> customerPurchases)
+        {
+            if (customerPurchases.Count == 0)
+                return (new HashSet<string>(), new HashSet<string>(), allFactories);
+
+            var purchasedByAll = new HashSet<string>(allFactories);
+            var purchasedBySome = new HashSet<string>();
+
+            foreach (var purchases in customerPurchases.Values)
             {
-                if (value >= 60)
-                    throw new ArgumentException("Минуты должны быть в диапазоне 0-59");
-                _minutes = value;
+                purchasedByAll.IntersectWith(purchases);
+                purchasedBySome.UnionWith(purchases);
             }
+
+            var purchasedByNone = new HashSet<string>(allFactories);
+            purchasedByNone.ExceptWith(purchasedBySome);
+
+            return (purchasedByAll, purchasedBySome, purchasedByNone);
+        }
+
+        // ЗАДАНИЕ 4.7 - HashSet с текстом
+        public static IEnumerable<char> AnalyzeRussianText(string text)
+        {
+            char[] deafConsonants = { 'п', 'ф', 'к', 'т', 'ш', 'с', 'х', 'ц', 'ч', 'щ' };
+            
+            var words = text.ToLower().Split(new[] { ' ', ',', '.', '!', '?', ';', ':', '\n', '\r', '\t' },
+                StringSplitOptions.RemoveEmptyEntries);
+            
+            var nchetWordsChars = new HashSet<char>();
+            var chetWordsChars = new HashSet<char>();
+            
+            for (int i = 0; i < words.Length; i++)
+            {
+                foreach (char c in words[i])
+                {
+                    if (!char.IsLetter(c)) continue;
+                    
+                    if ((i + 1) % 2 == 1)
+                    {
+                        nchetWordsChars.Add(c);
+                    }
+                    else
+                    {
+                        chetWordsChars.Add(c);
+                    }
+                }
+            }
+            
+            var result = new HashSet<char>();
+            foreach (char consonant in deafConsonants)
+            {
+                if (nchetWordsChars.Contains(consonant) && !chetWordsChars.Contains(consonant))
+                {
+                    result.Add(consonant);
+                }
+            }
+            
+            var sortedResult = new List<char>(result);
+            sortedResult.Sort();
+            return sortedResult;
+        }
+
+        // ЗАДАНИЕ 5.7 - Dictionary
+        
+        public static void FillDataFile(string filePath, List<string> data)
+        {
+            File.WriteAllLines(filePath, data);
         }
         
-        public Time(byte hours, byte minutes)
+        public static (int count15, int count20, int count25) AnalyzeSourCreamPrices(string filePath)
         {
-            Hours = hours;
-            Minutes = minutes;
-        }
+            var lines = File.ReadAllLines(filePath);
+            
+            var minPrices = new Dictionary<int, (int minPrice, int count)>
+            {
+                { 15, (int.MaxValue, 0) },
+                { 20, (int.MaxValue, 0) },
+                { 25, (int.MaxValue, 0) }
+            };
 
-        public Time() : this(0, 0) { }
+            foreach (var line in lines)
+            {
+                var parts = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                if (parts.Length == 4 && int.TryParse(parts[2], out int fat) && 
+                    int.TryParse(parts[3], out int price))
+                {
+                    if (minPrices.ContainsKey(fat))
+                    {
+                        var current = minPrices[fat];
+                        
+                        if (price < current.minPrice)
+                        {
+                            minPrices[fat] = (price, 1);
+                        }
+                        else if (price == current.minPrice)
+                        {
+                            minPrices[fat] = (current.minPrice, current.count + 1);
+                        }
+                    }
+                }
+            }
+            
+            int count15 = minPrices[15].minPrice != int.MaxValue ? minPrices[15].count : 0;
+            int count20 = minPrices[20].minPrice != int.MaxValue ? minPrices[20].count : 0;
+            int count25 = minPrices[25].minPrice != int.MaxValue ? minPrices[25].count : 0;
 
-        // ЗАДАНИЕ 6.7 - Метод вычитания
-        public Time Subtract(Time other)
-        {
-            if (other == null)
-                throw new ArgumentNullException(nameof(other));
-
-            int totalMinutes = (Hours * 60 + Minutes) - (other.Hours * 60 + other.Minutes);
-            
-            if (totalMinutes < 0)
-                totalMinutes += 24 * 60;
-            
-            return new Time((byte)(totalMinutes / 60), (byte)(totalMinutes % 60));
-        }
-
-        // ЗАДАНИЕ 7.7 - Перегрузка операторов
-        
-        public static Time operator ++(Time t)
-        {
-            if (t == null) throw new ArgumentNullException(nameof(t));
-            
-            Time original = new Time(t.Hours, t.Minutes);
-            
-            int totalMinutes = (t.Hours * 60 + t.Minutes) + 1;
-            totalMinutes %= (24 * 60);
-    
-            t.Hours = (byte)(totalMinutes / 60);
-            t.Minutes = (byte)(totalMinutes % 60);
-            
-            return original;
-        }
-
-        public static Time operator --(Time t)
-        {
-            if (t == null) throw new ArgumentNullException(nameof(t));
-            
-            Time original = new Time(t.Hours, t.Minutes);
-            
-            int totalMinutes = (t.Hours * 60 + t.Minutes) - 1;
-            if (totalMinutes < 0)
-                totalMinutes += 24 * 60;
-    
-            t.Hours = (byte)(totalMinutes / 60);
-            t.Minutes = (byte)(totalMinutes % 60);
-            
-            return original;
-        }
-        
-        public static explicit operator bool(Time t)
-        {
-            if (t == null) throw new ArgumentNullException(nameof(t));
-            return t.Hours != 0 || t.Minutes != 0;
-        }
-
-        public static implicit operator int(Time t)
-        {
-            if (t == null) throw new ArgumentNullException(nameof(t));
-            return t.Hours * 60 + t.Minutes;
-        }
-        
-        public static bool operator <(Time t1, Time t2)
-        {
-            if (t1 == null || t2 == null) 
-                throw new ArgumentNullException();
-            return (int)t1 < (int)t2;
-        }
-
-        public static bool operator >(Time t1, Time t2)
-        {
-            if (t1 == null || t2 == null) 
-                throw new ArgumentNullException();
-            return (int)t1 > (int)t2;
-        }
-        
-        public static Time operator +(Time t, uint minutes)
-        {
-            if (t == null) throw new ArgumentNullException(nameof(t));
-            
-            int totalMinutes = (int)t + (int)minutes;
-            totalMinutes %= (24 * 60);
-            if (totalMinutes < 0) totalMinutes += 24 * 60;
-            
-            return new Time((byte)(totalMinutes / 60), (byte)(totalMinutes % 60));
-        }
-
-        public static Time operator -(Time t, uint minutes)
-        {
-            if (t == null) throw new ArgumentNullException(nameof(t));
-            
-            int totalMinutes = (int)t - (int)minutes;
-            totalMinutes %= (24 * 60);
-            if (totalMinutes < 0) totalMinutes += 24 * 60;
-            
-            return new Time((byte)(totalMinutes / 60), (byte)(totalMinutes % 60));
-        }
-
-        public static Time operator +(Time t1, Time t2)
-        {
-            if (t1 == null || t2 == null) 
-                throw new ArgumentNullException();
-            return t1 + (uint)((int)t2);
-        }
-
-        public static Time operator -(Time t1, Time t2)
-        {
-            if (t1 == null || t2 == null) 
-                throw new ArgumentNullException();
-            return t1 - (uint)((int)t2);
-        }
-        
-        public override string ToString()
-        {
-            return $"{Hours:D2}:{Minutes:D2}";
-        }
-        
-        public override bool Equals(object obj)
-        {
-            return obj is Time time &&
-                   Hours == time.Hours &&
-                   Minutes == time.Minutes;
-        }
-
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(Hours, Minutes);
+            return (count15, count20, count25);
         }
     }
 }
